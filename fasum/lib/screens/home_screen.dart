8f013959bdb3f71ed930e9efb945dbc16ba0cf67
Callisyaaaaa <160854587+Callisyaaaaa@users.noreply.github.com>
2025-04-25 06:30:45 -0700

    fasum lanjut
    fasum lanjut

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,11 +14,11 @@ class HomeScreen extends StatelessWidget {
     if (diff.inSeconds < 60) {
       return '${diff.inSeconds} secs ago';
     } else if (diff.inMinutes < 60) {
-      return '${diff.inMinutes} mins ago';
+      return '${diff.inMinutes} min ago';
     } else if (diff.inHours < 24) {
-      return '${diff.inHours} hrs ago';
+      return '${diff.inHours} hour ago';
     } else {
-      return DateFormat('dd/mm/yyyy').format(dateTime);
+      return DateFormat('dd/MM/yyyy').format(dateTime);
     }
   }
 
@@ -44,27 +45,58 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection("posts")
-              .orderBy("created", descending: true)
-              .snapshots(),
-          builder: (context, snapshots) {
-            if (snapshots.hasData)
-              return Center(child: CircularProgressIndicator());
+        stream: FirebaseFirestore.instance
+            .collection("posts")
+            .
+            //.orderBy('createdAt',descending: true).
+            snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
 
-            final posts = snapshots.data!.docs;
-            return ListView.builder(
-                itemCount: posts.length,
-                itemBuilder: (context, index) {
-                  final data = posts[index].data();
-                  final imageBase64 = data["image"];
-                  final description = data["description"];
-                  final createdAtStr = data["createdAt"];
-                  final fullName = data["fullName"] ?? 'Anonim';
+          final posts = snapshot.data!.docs;
 
-                  final createdAt = DateTime.parse(createdAtStr);
-                });
-          }),
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final data = posts[index].data();
+              //final imageBase64 = data['image'];
+              final description = data['description'];
+              //final createdAtStr = data['createdAt'];
+              final fullName = data['fullName'] ?? 'Anonim';
+
+              //parse ke datetime
+              //final createdAt = DateTime.parse(createdAtStr);
+              return Card(
+                margin: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      fullName,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      description ?? '',
+                      style: const TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
     );
   }
 }

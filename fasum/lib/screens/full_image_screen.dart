685a1ddecf365d8142e51970ff58cd973440c3db
Callisyaaaaa <160854587+Callisyaaaaa@users.noreply.github.com>
2025-05-14 17:25:26 +0700

    commit
    commit

---
+++ fasum/lib/screens/full_image_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class FullImageScreen extends StatelessWidget {
+  final String imagebased64;
+  const FullImageScreen({
+    super.key,
+    required this.imagebased64,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: InteractiveViewer(
+              child: Image.memory(
+            base64Decode(imagebased64),
+            fit: BoxFit.contain,
+          )),
+        ),
+      ),
+    );
+  }
+}
--- fasum/lib/screens/sign_up_screen.dart
+++ fasum/lib/screens/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
@@ -12,6 +13,7 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _fullNamecontroller = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
@@ -26,6 +28,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextField(
+              controller: _fullNamecontroller,
+              decoration: const InputDecoration(labelText: 'Full Name'),
+            ),
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(labelText: 'Email'),
@@ -67,10 +73,21 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
 
     try {
+      final newUser = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text);
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(newUser.user!.uid)
+          .set({
+        'fullname': _fullNamecontroller,
+        'email': _emailController,
+        'createdAt': Timestamp.now()
+      });
       if (mounted) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
